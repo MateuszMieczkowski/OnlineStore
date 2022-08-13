1f1936b4f6653dca5f2a34bc58ccfe95292eccef
Michał Jabłoński <jblnsky27@gmail.com>
2022-08-14 01:34:45 +0200

    refactored size page to patrial view (size creator, size editor, size viewer) + code behind in main size page

---
+++ Client/Pages/Size.razor.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using SneakersBase.Client.Services;
+using SneakersBase.Shared.Models;
+
+namespace SneakersBase.Client.Pages;
+
+public class SizeBase : ComponentBase
+{
+    #region Injections
+    [Inject]
+    protected ToastService Toast { get; set; }
+    [Inject]
+    protected ISizeService SizeService { get; set; }
+    [Inject]
+    protected IJSRuntime JsRuntime { get; set; }
+    #endregion
+
+    #region Properties
+    protected List<SizeDto>? sizes;
+    protected UpdateSizeDto? EditSize { get; set; }
+    protected SizeDto ConfirmSize { get; set; }
+    protected CreateSizeDto CreateSize { get; set; }
+    #endregion
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        sizes = await SizeService.GetAllAsync();
+        CreateSize = new();
+    }
+
+    protected async void UpdateSize(SizeDto sizeDto)
+    {
+        try
+        {
+            ConfirmSize = sizeDto;
+            EditSize = new UpdateSizeDto()
+            {
+                Name = sizeDto.Name
+            };
+            await JsRuntime.InvokeVoidAsync("ScrollTop");
+            StateHasChanged();
+        }
+        catch (Exception ex)
+        {
+            Toast.ShowToast(ex.Message, ToastLevel.Error);
+        }
+        
+    }
+    protected async void SaveEdit(UpdateSizeDto updateSizeDto)
+    {
+        try
+        {
+            var updatedSize = await SizeService.Update(ConfirmSize.Id, updateSizeDto);
+
+            ConfirmSize.Name = updatedSize.Name;
+            Toast.ShowToast("Size updated!", ToastLevel.Success);
+            StateHasChanged();
+        }
+        catch (Exception ex)
+        {
+            Toast.ShowToast(ex.Message, ToastLevel.Error);
+        }
+
+    }
+
+    protected async void RemoveSize(SizeDto sizeDto)
+    {
+
+        try
+        {
+            await SizeService.Remove(sizeDto.Id);
+
+            sizes?.Remove(sizeDto);
+            Toast.ShowToast("Size removed!", ToastLevel.Success);
+            StateHasChanged();
+        }
+        catch (Exception ex)
+        {
+            Toast.ShowToast("Something went wrong: " + ex.Message, ToastLevel.Error);
+        }
+    }
+
+
+
+    protected async void AddSize(CreateSizeDto sizeDto)
+    {
+        try
+        {
+            var newSize = await SizeService.Create(sizeDto);
+            sizes?.Add(newSize);
+            Toast.ShowToast("Size added!", ToastLevel.Success);
+
+            sizeDto.Name = string.Empty; ;
+            StateHasChanged();
+        }
+        catch (Exception ex)
+        {
+            Toast.ShowToast("Something went wrong: " + ex.Message, ToastLevel.Error);
+        }
+    }
+
+}
