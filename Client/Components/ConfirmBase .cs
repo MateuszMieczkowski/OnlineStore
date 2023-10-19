c2ec68fc8a3822e678110bebaa96d6a6df96623c
Michał Jabłoński <jblnsky27@gmail.com>
2023-10-19 18:01:57 +0200

    changed some config

--- Client/Components/ConfirmBase .cs
+++ Client/Components/ConfirmBase .cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace SneakersBase.Client.Components
 {
@@ -30,7 +25,6 @@ namespace SneakersBase.Client.Components
         {
             ShowConfirmation = false;
             await ConfirmationChanged.InvokeAsync(value);
-
         }
     }
 }
