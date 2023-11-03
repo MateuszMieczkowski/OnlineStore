02084d999124f5d8be3b116b41e17c2b1cec7d62
Mateusz Mieczkowski <mieczkowski.mateusz@outlook.com>
2023-11-03 23:06:47 +0100

    Remove services

--- Server/Controllers/SizeController.cs
+++ Server/Controllers/SizeController.cs
@@ -10,35 +10,34 @@ namespace OnlineStore.Server.Controllers;
 [Route("api/size")]
 public class SizeController : ControllerBase
 {
-    private readonly ISizeService _sizeService;
+ 
 
-    public SizeController(ISizeService sizeService)
+    public SizeController()
     {
-        _sizeService = sizeService;
+
     }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<SizeDto>>> GetAll()
     {
-        return Ok(await _sizeService.GetAll());
+        return Ok();
     }
 
     [HttpPost]
     public ActionResult<SizeDto> Create([FromBody] CreateSizeDto dto)
     {
-        return Created("api/size", _sizeService.Create(dto));
+        return Created("api/size", null);
     }
 
     [HttpPut("{id}")]
     public ActionResult<SizeDto> Update([FromRoute] Guid id, [FromBody] UpdateSizeDto dto)
     {
-        return Ok(_sizeService.Update(id, dto));
+        return Ok();
     }
 
     [HttpDelete("{id}")]
     public ActionResult Remove([FromRoute] Guid id)
     {
-        _sizeService.Remove(id);
         return NoContent();
     }
 }
