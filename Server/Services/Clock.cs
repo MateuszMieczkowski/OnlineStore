95f1e4111e4c4c8e0da1ecd8676decba6f9f4ac6
Mateusz Mieczkowski <mieczkowski.mateusz@outlook.com>
2023-11-07 20:14:08 +0100

    Add clock interface

---
+++ Server/Services/Clock.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineStore.Server.Services
+{
+    public interface IClock
+    {
+        DateTimeOffset UtcNow { get; }
+    }
+
+    public class Clock : IClock
+    {
+        public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
+    }   
+}
