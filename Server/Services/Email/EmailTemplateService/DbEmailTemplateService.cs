6ac6a40160d24e049eee4331cc77ccde016c10dc
Mateusz Mieczkowski <mieczkowski.mateusz@outlook.com>
2023-11-08 23:17:45 +0100

    Add Email template service with proxy and factory

---
+++ Server/Services/Email/EmailTemplateService/DbEmailTemplateService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Server.Entities;
+using OnlineStore.Server.Services.Exceptions;
+
+namespace OnlineStore.Server.Services.Email.EmailTemplateService;
+
+public class DbEmailTemplateService : IEmailTemplateService
+{
+    private readonly OnlineStoreDbContext _dbContext;
+
+    public DbEmailTemplateService(OnlineStoreDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<string> GetEmailTemplateAsync(string templateName, CancellationToken cancellationToken = default)
+    {
+        var template = await _dbContext.EmailTemplates
+            .AsNoTracking()
+            .Where(e => e.Name == templateName)
+            .Select(e => e.HtmlContent)
+            .FirstOrDefaultAsync(cancellationToken) ??
+                throw new NotFoundException($"Email template with name {templateName} not found");
+
+        return template;
+    }
+
+    public async Task AddEmailTemplateAsync(string templateName, string templateContent,
+        CancellationToken cancellationToken = default)
+    {
+        _dbContext.EmailTemplates.Add(new EmailTemplate
+        {
+            Name = templateName,
+            HtmlContent = templateContent
+        });
+
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ Server/Services/Email/EmailTemplateService/EmailTemplateServiceFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace OnlineStore.Server.Services.Email.EmailTemplateService;
+
+public interface IEmailTemplateServiceFactory
+{
+    IEmailTemplateService Create();
+}
+
+
+public class EmailTemplateServiceFactory : IEmailTemplateServiceFactory
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public EmailTemplateServiceFactory(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public IEmailTemplateService Create()
+    {
+        return _serviceProvider.GetRequiredService<CachedEmailTemplateService>();
+    }
+}
