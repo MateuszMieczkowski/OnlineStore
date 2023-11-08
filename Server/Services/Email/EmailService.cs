79cd98caef4c0497542c6f89da4abb7e2317f8e5
Mateusz Mieczkowski <mieczkowski.mateusz@outlook.com>
2023-11-08 23:18:21 +0100

    Add email service

---
+++ Server/Services/Email/EmailService.cs
@@ -0,0 +1,28 @@
+﻿using OnlineStore.Server.Emails.TemplateDefinitions;
+
+namespace OnlineStore.Server.Services.Email
+{
+    public class EmailService : IEmailService
+    {
+        private readonly OnlineStoreDbContext _dbContext;
+        private readonly IEmailBuilderFactory _emailBuilderFactory;
+
+        public EmailService(OnlineStoreDbContext dbContext, IEmailBuilderFactory emailBuilderFactory)
+        {
+            _dbContext = dbContext;
+            _emailBuilderFactory = emailBuilderFactory;
+        }
+
+        public async Task SendEmailFromTemplateAsync(EmailTemplateDefinition templateDefinition,
+            CancellationToken cancellationToken = default)
+        {
+            var builder = _emailBuilderFactory.Create();
+
+            var email = await builder.FromTemplate(templateDefinition)
+                .BuildAsync(cancellationToken);
+
+            _dbContext.Emails.Add(email);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ Server/Services/Email/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using OnlineStore.Server.Emails.TemplateDefinitions;
+
+namespace OnlineStore.Server.Services.Email
+{
+    public interface IEmailService
+    {
+        public Task SendEmailFromTemplateAsync(EmailTemplateDefinition templateDefinition, CancellationToken cancellationToken = default);
+    }
+}
