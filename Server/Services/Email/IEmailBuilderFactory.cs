21927a8b30f55323732e14f1658f9a8482d26fc6
Mateusz Mieczkowski <mieczkowski.mateusz@outlook.com>
2023-11-08 23:18:43 +0100

    Add email builder factory

---
+++ Server/Services/Email/IEmailBuilderFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace OnlineStore.Server.Services.Email;
+
+public interface IEmailBuilderFactory
+{
+    IEmailBuilder Create();
+}
+
+public class EmailBuilderFactory : IEmailBuilderFactory
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public EmailBuilderFactory(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public IEmailBuilder Create()
+    {
+        return _serviceProvider.GetRequiredService<EmailBuilder>();
+    }
+}   
