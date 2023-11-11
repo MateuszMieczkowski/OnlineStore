164ef336a02c24cb038a4d31b04260c132a52236
Mateusz Mieczkowski <mieczkowski.mateusz@outlook.com>
2023-11-11 12:49:26 +0100

    Add email definiton class

---
+++ Server/Emails/TemplateDefinitions/EmailDefinition.cs
@@ -0,0 +1,22 @@
+﻿namespace OnlineStore.Server.Emails.TemplateDefinitions;
+
+public abstract class EmailDefinition
+{
+    protected EmailDefinition(string recipientEmail, string? recipientName, string? senderEmail)
+    {
+        RecipientEmail = recipientEmail;
+        RecipientName = recipientName;
+        SenderEmail = senderEmail;
+    }
+
+    public string? SenderEmail { get; private set; }
+    public string RecipientEmail { get; private set; }
+    public string? RecipientName { get; private set; }
+
+    public abstract string Subject { get; }
+    public abstract string TemplateName { get; }
+    public abstract ICollection<EmailReplacement> GetReplacements();
+}
+
+public record EmailReplacement(string Key, string Value);
+
