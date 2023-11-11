e49aa2921061b32e17aa70bdb0d09bf4bfe02546
Mateusz Mieczkowski <mieczkowski.mateusz@outlook.com>
2023-11-11 12:48:31 +0100

    Add email builder

---
+++ Server/Services/Email/Builder/IEmailBuilder.cs
@@ -0,0 +1,14 @@
+﻿using OnlineStore.Server.Emails.TemplateDefinitions;
+
+namespace OnlineStore.Server.Services.Email.Builder;
+
+public interface IEmailBuilder
+{
+    IEmailBuilder AddSubject(string subject);
+    IEmailBuilder AddRecipientEmail(string email);
+    IEmailBuilder AddRecipientName(string name);
+    IEmailBuilder AddSenderEmail(string email);
+    IEmailBuilder AddHtmlBody(string htmlBody);
+    IEmailBuilder FromTemplate(EmailDefinition emailDefinition);
+    Task<Entities.Email> BuildAsync(CancellationToken cancellationToken = default);
+}
---
+++ Server/Services/Email/Builder/IEmailBuilderFactory.cs
@@ -0,0 +1,24 @@
+﻿using OnlineStore.Server.Services.Email.EmailTemplateService;
+
+namespace OnlineStore.Server.Services.Email.Builder;
+
+public interface IEmailBuilderFactory
+{
+    IEmailBuilder Create();
+}
+
+public class EmailBuilderFactory : IEmailBuilderFactory
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public EmailBuilderFactory(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public IEmailBuilder Create()
+    {
+        var emailTemplateServiceFactory =  _serviceProvider.GetRequiredService<IEmailTemplateServiceFactory>();
+        return new EmailBuilder(emailTemplateServiceFactory);
+    }
+}
