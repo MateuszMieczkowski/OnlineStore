c24975bb28a492dfd1084333cd46aaac085bf3de
Mateusz Mieczkowski <mieczkowski.mateusz@outlook.com>
2023-11-22 19:30:42 +0100

    Add CreateProductBatch command, handler and validator

---
+++ Server/Products/Validators/CreateProductsBatchValidator.cs
@@ -0,0 +1,70 @@
+﻿using FluentValidation;
+using OnlineStore.Shared.Products;
+
+namespace OnlineStore.Server.Products.Validators;
+
+public class CreateProductsBatchValidator : AbstractValidator<CreateProductsBatch>
+{
+    public CreateProductsBatchValidator()
+    {
+        RuleForEach(x => x.Products)
+            .SetValidator(new CreateProductDtoValidator());
+    }
+}
+
+public class CreateProductDtoValidator : AbstractValidator<CreateProductDto>
+{
+    public CreateProductDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(255);
+        
+        RuleFor(x => x.ShortDescription)
+            .NotEmpty()
+            .MaximumLength(500);
+        
+        RuleFor(x => x.ReferenceNumber)
+            .NotEmpty()
+            .MaximumLength(50);
+        
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .MaximumLength(2000);
+        
+        RuleFor(x => x.Quantity)
+            .NotEmpty()
+            .GreaterThan(0);
+        
+        RuleFor(x => x.PriceNet)
+            .NotEmpty()
+            .GreaterThan(0);
+        
+        RuleFor(x => x.PriceGross)
+            .NotEmpty()
+            .GreaterThan(0);
+
+        RuleFor(x => x.TaxRateId)
+            .NotEmpty();
+        
+        RuleForEach(x => x.ProductFiles)
+            .NotEmpty()
+            .SetValidator(new CreateProductFileValidator());
+    }
+}
+
+public class CreateProductFileValidator : AbstractValidator<CreateProductFile>
+{
+    public CreateProductFileValidator()
+    {
+        RuleFor(x => x.FileName)
+            .NotEmpty()
+            .MaximumLength(255);
+        
+        RuleFor(x => x.FileBase64)
+            .NotEmpty();
+        
+        RuleFor(x => x.ProductFileType)
+            .IsInEnum();
+    }
+}
---
+++ Shared/Products/CreateProductBatch.cs
@@ -0,0 +1,19 @@
+﻿using OnlineStore.Shared.Infrastructure;
+
+namespace OnlineStore.Shared.Products;
+
+public record CreateProductsBatch(IReadOnlyCollection<CreateProductDto> Products) : ICommand;
+
+public record CreateProductDto(
+    string Name,
+    string ReferenceNumber,
+    string? ShortDescription,
+    string Description,
+    int Quantity,
+    decimal PriceNet,
+    decimal PriceGross,
+    bool IsHidden,
+    int TaxRateId,
+    IReadOnlyCollection<CreateProductFile> ProductFiles);
+    
+public record CreateProductFile(string FileName, string FileBase64, ProductFileTypeDto ProductFileType, string? Description);
