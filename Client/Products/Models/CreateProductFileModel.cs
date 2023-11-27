36b72bc3de16f624f4129d8baaa30a356093b8d8
Michał Jabłoński <jblnsky27@gmail.com>
2023-11-27 22:08:27 +0100

    fixed generic component

--- Client/Products/Models/CreateProductFileModel.cs
+++ Client/Products/Models/CreateProductFileModel.cs
@@ -5,13 +5,4 @@ namespace OnlineStore.Client.Products.Models;
 
 public class CreateProductFileModel : ProductFileModel
 {
-    // public string FileName { get; set; }
-
-    // public IBrowserFile File { get; set; }
-
-    // public string FileBase64 { get; set; }
-    
-    // public ProductFileTypeDto ProductFileType { get; set; }
-
-    // public string? Description { get; set; }
 }
